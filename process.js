214ae367db645a5a215e5b24806fd1177bd926ab
Elizabeth <lizabulat888@gmail.com>
2026-04-29 22:15:14 +0300

    process.js

--- process.js
+++ process.js
@@ -18,3 +18,10 @@ function setActive(index) {
     gifs[index].classList.add("active");
 }
 </script>
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        document.body.classList.add("scrolled");
+    } else {
+        document.body.classList.remove("scrolled");
+    }
+});
